cd254664080e7add8e766be38da3d1ad18c2b336
Reb Debugs <rebeccamariebarrett@gmail.com>
2023-12-10 19:44:32 +0000

    Created Functions to Facilitate One Round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
 function roundOne(){
     let computerSelection = getComputerChoice();
-    console.log(computerSelection);
     let playerSelection = prompt('Enter Your Choice:').toUpperCase();
-    console.log(playerSelection);
+    switch (playerSelection){
+        case 'ROCK':
+            switch (computerSelection){
+                case 'ROCK':
+                    console.log('Tie, try again');
+                    roundOne();
+                
+                case 'PAPER':
+                    console.log('Computer Wins');
+                    break;
+
+                case 'SCISSORS':
+                    console.log('Player Wins');
+                    break;
+            
+            }
+            break;
+
+        case 'PAPER':
+            switch (computerSelection){
+                case 'ROCK':
+                    console.log('Player Wins');
+                    roundOne();
+                
+                case 'PAPER':
+                    console.log('Tie, try again');
+                    break;
+
+                case 'SCISSORS':
+                    console.log('Computer Wins');
+                    break;
+
+            }
+            break;
+
+        case 'SCISSORS':
+            switch (computerSelection){
+                case 'ROCK':
+                    console.log('Computer Wins');
+                    roundOne();
+                
+                case 'PAPER':
+                    console.log('Player Wins');
+                    break;
+
+                case 'SCISSORS':
+                    console.log('Tie, try again');
+                    break;
+
+            }
+            break;
+            
+
+        default:
+            console.log('Invalid Input');
+            roundOne();
+    }
+
+
 }
 
 function getComputerChoice(){
